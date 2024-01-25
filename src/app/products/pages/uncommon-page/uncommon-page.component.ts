5dc0e9e692d044aa4520ece9bdba9aa2cd992ed6
Jose Ramirez <jramireztolentino@gmail.com>
2024-01-25 16:52:57 -0400

    Add the pipes angular number, percent, i18nSelect, i18nPlural, keyvalue, slice, json, async

--- src/app/products/pages/uncommon-page/uncommon-page.component.ts
+++ src/app/products/pages/uncommon-page/uncommon-page.component.ts
@@ -7,4 +7,40 @@ import { Component } from '@angular/core';
 })
 export class UncommonPageComponent {
 
+  // i18n Select
+  public name: string = "Jose";
+  public gender: 'male' | 'female' = 'male';
+
+  public invitationMap = {
+    male: 'invitarlo',
+    female: 'invitarla'
+  }
+
+  changeClient(): void {
+    this.name = 'Valeryn';
+    this.gender = 'female'
+  }
+
+  // i18nPlural
+  public clients: string[] = [
+    'Maria','Pedro','Fernando','Hernando','Eduardo','Melissa','Natalia',
+  ]
+  public clientsMap = {
+    '=0':'no tenemos ningún cliente esperando.',
+    '=1':'tenemos un cliente esperando',
+    'other': 'tenemos # clientes esperando'
+  }
+
+  deleteClient():void {
+    this.clients.shift();
+  }
+
+  // KeyValue Pipe
+  public person = {
+    name: 'Jose',
+    age: 36,
+    address: 'San Antonio, Los Alcarrizos'
+  }
+
+
 }
--- src/app/shared/components/menu/menu.component.ts
+++ src/app/shared/components/menu/menu.component.ts
@@ -28,4 +28,3 @@ export class MenuComponent implements OnInit{
     ]
   }
  }
- 
